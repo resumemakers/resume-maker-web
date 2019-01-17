b1369beba2e1ce2f1c74a2867ee9f0a9dd71e547
Renan Gomes <renan.gomes@live.com>
2019-01-17 01:20:11 -0200

    setup configuration for tests using mocha/chai for typescript code.

---
+++ static/tests/basic-config.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import 'mocha'
+
+describe('Typescript test basic configuration', () => {
+    it('should works', () => {
+        expect('test').to.equal('test')
+    })
+})
