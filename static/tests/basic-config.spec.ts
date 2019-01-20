c4ad1a0e522be5280eb776215e537b9069f9e140
Renan Gomes <renan.gomes@live.com>
2019-01-20 17:04:37 -0200

    GH-7 - Adicionando modelo contendo os atributos que devem ser enviados para API

--- static/tests/basic-config.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai'
-import 'mocha'
-
-describe('Typescript test basic configuration', () => {
-    it('should works', () => {
-        expect('test').to.equal('test')
-    })
-})
